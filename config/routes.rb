e140564345b615fb128067f9b0b0cbb58fee59ec
chisaton <anderluehe13dsy4430@gmail.com>
2016-08-02 17:05:24 +0900

    add sign up function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'messages#index'
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
   namespace :api do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create ]
   end
 end
