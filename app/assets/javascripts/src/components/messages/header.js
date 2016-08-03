fe7bcbe2f8f8103de745b3c99a8926dda098015d
chisaton <anderluehe13dsy4430@gmail.com>
2016-08-03 12:14:34 +0900

    implement devise view

--- app/assets/javascripts/src/components/messages/header.js
+++ app/assets/javascripts/src/components/messages/header.js
@@ -1,12 +1,12 @@
-import React from 'react'
-
-class Header extends React.Component {
-  render() {
-    return (
-        <header className='header'>
-        </header>
-      )
-  }
-}
-
-export default Header
+// import React from 'react'
+//
+// class Header extends React.Component {
+//   render() {
+//     return (
+//         <header className='header'>
+//         </header>
+//       )
+//   }
+// }
+//
+// export default Header
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'messages#index'
+  # root 'messages#index'
+  devise_scope :user do
+    root :to => "devise/registrations#new" #ログイン画面
+  end
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   namespace :api do
