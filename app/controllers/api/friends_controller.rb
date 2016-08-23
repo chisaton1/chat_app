65165592d09fcacf32dddcc927876bf0c4ee765f
chisaton <anderluehe13dsy4430@gmail.com>
2016-08-23 13:12:32 +0900

    add two methods in Users model

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -1,8 +1,9 @@
 class Api::FriendsController < ApplicationController
   def index
-    ids = Friend.where(user_id: current_user.id).pluck(:to_user_id)
-    ids << Friend.where(to_user_id: current_user.id).pluck(:user_id)
+    ids = current_user.friend_ids
+    # ids = Friend.where(user_id: current_user.id).pluck(:to_user_id)
+    # ids << Friend.where(to_user_id: current_user.id).pluck(:user_id)
     # チャットをしている友人のデータのみをjsonで返す
-    render json: User.where(id: ids.flatten)
+    render json: User.where(id: ids)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ActiveRecord::Base
     Friend.find_by(user_id: self.id, to_user_id: user.id) ||
     Friend.find_by(user_id: user.id, to_user_id: self.id)
   end
+
+  def friend_data_for(user)
+    Friend.find_by(user_id: self.id, to_user_id: user.id) ||
+      Friend.find_by(user_id: user.id, to_user_id: self.id)
+  end
+
+  def friend_ids
+    ids1 = Friend.where(user_id: self.id).pluck(:to_user_id)
+    ids2 = Friend.where(to_user_id: self.id).pluck(:user_id)
+    (ids1 << ids2).flatten
+  end
 end
